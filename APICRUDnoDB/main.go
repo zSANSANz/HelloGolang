b88e5b92cffbb95074c64d8729da8d8b75ec7752
unknown <sandipermanasoebagio@gmail.com>
2020-11-14 12:19:41 +0700

    menambahkan handler untuk get by id dan menambahkan routenya

--- APICRUDnoDB/main.go
+++ APICRUDnoDB/main.go
@@ -25,11 +25,24 @@ func allProducts(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Products)
 }
 
+func singleProducts(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+
+	for _, product := range Products {
+		if product.ID == id {
+			json.NewEncoder(w).Encode(product)
+		}
+	}
+}
+
 func handleRequest() {
 	r := mux.NewRouter().StrictSlash(true)
 
 	r.HandleFunc("/", home)
 	r.HandleFunc("/products", allProducts)
+	r.HandleFunc("/products/{id}", singleProducts)
+
 	// pesan kalau aplikasi berjalan
 	fmt.Println("Application running")
 	log.Fatal(http.ListenAndServe(":8000", r))
@@ -37,8 +50,8 @@ func handleRequest() {
 
 func main() {
 	Products = []Product{
-		Product{Title: "First Product", Price: 2000000, Quantity: 5},
-		Product{Title: "Second Product", Price: 500000, Quantity: 15},
+		Product{ID: "1", Title: "First Product", Price: 2000000, Quantity: 5},
+		Product{ID: "2", Title: "Second Product", Price: 500000, Quantity: 15},
 	}
 	handleRequest()
 }
