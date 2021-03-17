6509dba372ab1b63ac1e2de34d9a6fc2f3a0f01c
unknown <sandipermanasoebagio@gmail.com>
2021-03-17 10:13:08 +0700

    soal part 2 dan 3 but not fixed

--- alterras/c01/partiii/Soal/3/main.go
+++ alterras/c01/partiii/Soal/3/main.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 )
 
-func ArrayMerge(arrayA, arrayB []string) []string] {
+func ArrayMerge(arrayA, arrayB []string) []string {
+	return append([]string{"1","2"}, []string{"3","4"})
 }
 
 func main() {
-	fmt.Println(ArrayMerge([string{"king", "devil jin", "akuma"},
-	[]string{"eddie", "steve", "geese"}]))
+	fmt.Println(ArrayMerge([]string{"king", "devil jin", "akuma"}, []string{"eddie", "steve", "geese"}))
 }
